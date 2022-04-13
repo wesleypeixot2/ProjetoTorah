4d7f0e1a0735585fa0b83213a937c0ece665ce64
Wesley <leon.peixoto2@hotmail.com>
2022-04-13 18:33:28 -0300

    comitando todo o projeto da branch principal

---
+++ javascript/contato.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Toda Lecha, ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
